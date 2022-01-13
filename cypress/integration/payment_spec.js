802ff948b13a4bbab83611801d343a8e3a8bbbb8
cynthia <yinnycfu@gmail.com>
2022-01-13 18:17:37 +0000

    test: cypress test for user login

---
+++ cypress/integration/payment_spec.js
@@ -0,0 +1,20 @@
+describe("payment", () => {
+  it("user can make payment", () => {
+    // login
+    cy.visit("/");
+    cy.findByRole("textbox", { name: /username/i }).type("johndoe");
+    cy.findByLabelText(/password/i).type("s3cret");
+    cy.findByRole("checkbox", { name: /remember me/i }).check();
+    cy.findByRole("button", { name: /sign in/i }).click();
+
+    // check account balance
+    // click on pay button
+    // search for a user
+    // enter amount and note and click pay
+    // return to transactions
+    // go to personal payments (Mine)
+    // clicks on payment
+    // verify if payment was made
+    // verify if payment amount was deducted
+  })
+})
